ef41451948dc2eb3890d5b4ebd3177393cb73838
Omer Feldman <omerfeldman@microsoft.com>
2025-05-22 16:18:55 +0300

    added

--- IaC/app/chess_board.tf
+++ IaC/app/chess_board.tf
@@ -3,6 +3,7 @@ resource "local_file" "chess_board_config" {
   content = templatefile("${path.cwd}/${local.chess_directory_path}/.env.tftpl", {
     white_url = kubernetes_service.chess_white_agent.status[0].load_balancer[0].ingress[0].ip,
     black_url = kubernetes_service.chess_black_agent.status[0].load_balancer[0].ingress[0].ip,
-    otel_exporter_otlp_endpoint = kubernetes_service.otel_collector_lb.status[0].load_balancer[0].ingress[0].ip
+    otel_exporter_otlp_endpoint = kubernetes_service.otel_collector_lb.status[0].load_balancer[0].ingress[0].ip,
+    base_name = var.base_name
   })
 }
